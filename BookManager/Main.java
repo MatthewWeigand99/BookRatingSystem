dffa7cd479e04cac3382b99f38c168dc97317ab1
MatthewWeigand99 <mattweigand99@gmail.com>
2023-07-18 11:56:32 -0400

    Added TableViewer class and added SQL database variables to Main class

--- BookManager/Main.java
+++ BookManager/Main.java
@@ -4,16 +4,23 @@ import javax.swing.*;
 
 public class Main extends JFrame {
 
+    // MySQL Database Variables
+    private final String DB_URL = "jdbc:mysql://localhost:3306/book_manager";
+    private final String USER = "root";
+    private final String PASSWORD = "Mia!CompSci99";
+    private final String TABLE = "book_list_user";
+
     // Main frame variables
-    private String appName = "Book Rater";
+    private final String APP_TITLE = "Book Rater";
     private int appHeight = 600;
     private int appWidth = 800;
 
     private JPanel panel;
     private JButton exitButton;
+    private JButton showTableButton;
 
     public Main() {
-        setTitle(appName);
+        setTitle(APP_TITLE);
         setSize(appWidth, appHeight);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -28,11 +35,21 @@ public class Main extends JFrame {
 
     private void buildPanel() {
         exitButton = new JButton("Exit");
+        showTableButton = new JButton("Show Table");
 
         exitButton.addActionListener(new ExitButtonListener());
+        showTableButton.addActionListener(new ShowTableButtonListener());
 
         panel = new JPanel();
+        panel.add(showTableButton);
         panel.add(exitButton);
+    }
+
+    private class ShowTableButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            TableViewer tv = new TableViewer(DB_URL, USER, PASSWORD);
+            tv.viewTable(TABLE);
+        }
     }
 
     private class ExitButtonListener implements ActionListener {
